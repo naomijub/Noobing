5dc49e6fea59643004dac62ce63fe386db7f21ee
naomijub <jnboeira@outlook.com>
2017-01-31 15:21:42 -0300

    Noobing some more with strings

--- blaster-noobing/src/blaster_noobing/core.clj
+++ blaster-noobing/src/blaster_noobing/core.clj
@@ -4,4 +4,14 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Increment [1 2 3 4]: " (map inc [1 2 3 4]))
+  (println "Reduce + [20 30 50]: " (reduce + [20 30 50]))
+  (println "Reduce - [20 30 50]: " (reduce - [20 30 50]))
+  (println "Reduce / [20 30 50]: " (reduce / [20 30 50]))
+  (println "Crazy math (+ 1 (* 2 3) 2)" (+ 1 (* 2 3) 2))
+  (println "'Panda' + '3D' + 'Course' = " (str "Panda" "3D " "Course"))
+  (if (> 3 2)
+    (println "By Odin's bear!!")
+    (println "You crazy Joke!")
+  )
+)
