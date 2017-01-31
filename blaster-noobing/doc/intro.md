f56d2709078b7633007d71a5980bc1b4ba15a043
naomijub <jnboeira@outlook.com>
2017-01-31 12:03:01 -0300

    blasting noob level

---
+++ blaster-noobing/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to blaster-noobing
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ blaster-noobing/test/blaster_noobing/core_test.clj
@@ -0,0 +1,7 @@
+(ns blaster-noobing.core-test
+  (:require [clojure.test :refer :all]
+            [blaster-noobing.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
