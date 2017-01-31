1e23b0637c523640dd5741e784e4e99423e92480
naomijub <jnboeira@outlook.com>
2017-01-31 12:01:24 -0300

    Basic print and sum funcs

--- my-first-noob/src/my_first_noob/core.clj
+++ my-first-noob/src/my_first_noob/core.clj
@@ -4,4 +4,7 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, Naomijub! You are great!")
+  (do (println "Nothing awesome to say besides" (+ 3 4))
+  )
+)
