45ca280e78742568ddf15af4f7977ead0dca95ec
naomijub <jnboeira@outlook.com>
2017-01-31 11:43:55 -0300

    Lein proj

---
+++ my-first-noob/test/my_first_noob/core_test.clj
@@ -0,0 +1,7 @@
+(ns my-first-noob.core-test
+  (:require [clojure.test :refer :all]
+            [my-first-noob.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
