1120816e28cdf861fb780472d7d2e2dd1fe678ce
naomijub <jnboeira@outlook.com>
2017-02-21 10:48:56 -0300

    mapify vamp idx

--- fwpd/src/fwpd/core.clj
+++ fwpd/src/fwpd/core.clj
@@ -19,7 +19,18 @@
   (map #(clojure.string/split % #",")
        (clojure.string/split string #"\n")))
 
+(defn mapify
+  "Return a seq of maps"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+rows))
+
+
 (defn -main
   "FWPD vamps analyser"
   [& args]
-  (println (parse (slurp filename))))
+  (println (mapify (parse (slurp filename)))))
